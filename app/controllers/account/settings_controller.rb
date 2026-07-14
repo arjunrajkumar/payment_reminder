57aa1a7a4b3527197f82b121d7ea3784a6ec5e8a
arjun.rajkumar <arjunrajkumars@gmail.com>
2026-07-14 10:39:52 +0530

    CSS

--- app/controllers/account/settings_controller.rb
+++ app/controllers/account/settings_controller.rb
@@ -2,15 +2,11 @@ class Account::SettingsController < ApplicationController
   before_action :set_account
 
   def show
-    set_settings_dashboard
+    @invoice_sources = InvoiceSource.available_sources_for(@account)
   end
 
   private
     def set_account
       @account = Current.account
-    end
-
-    def set_settings_dashboard
-      @invoice_sources = InvoiceSource.available_sources_for(@account)
     end
 end
